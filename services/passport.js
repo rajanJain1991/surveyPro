30ef3f4cbc8b4956701c6bcb2c12637f730673db
Rajan Jain <jain.rajan91@gmail.com>
2018-02-23 18:33:46 +0100

    Added billing and client side app!

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser); //1st arg is for error, second arg i
-        } else {
-          //we don't have a user with this Id, add a new user
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser); //1st arg is for error, second arg i
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
